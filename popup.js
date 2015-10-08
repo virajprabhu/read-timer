096c55be5a235f10beaf2ba92ceb8a6e64394e4c
unknown <virprabh@VIRPRABH-W7-2.corp.adobe.com>
2015-10-08 14:21:43 +0530

    Integrated Readability. @bug: page reloads

--- popup.js
+++ popup.js
@@ -1,13 +1,15 @@
 chrome.extension.onRequest.addListener(function(wordLength) {
 	var averageReadSpeedWPM = 200;
-	var readTime = wordLength / averageReadSpeedWPM;
-	alert('Read Time' + readTime);
+	var readTime = Math.ceil(wordLength/averageReadSpeedWPM);
+	document.getElementById('status').innerHTML = readTime + " minute read.";
 });
 window.onload = function() {
 	chrome.windows.getCurrent(function (currentWindow) {
 		chrome.tabs.query({ active: true, windowId: currentWindow.id}, function(activeTabs){
-			chrome.tabs.executeScript(
-				activeTabs[0].id, {file: 'sendArticleLength.js', allFrames: true});
+			chrome.tabs.executeScript(null, { file: "readability/Readability.js" }, function() {
+				chrome.tabs.executeScript(
+					activeTabs[0].id, {file: 'sendArticleLength.js', allFrames: true});
+			});
 		});
 	});
 }
--- sendArticleLength.js
+++ sendArticleLength.js
@@ -1,9 +1,17 @@
 
-alert('Running!');
+var location = document.location;
+var uri = {
+	spec: location.href,
+	host: location.host,
+	prePath: location.protocol + "//" + location.host,
+	scheme: location.protocol.substr(0, location.protocol.indexOf(":")),
+	pathBase: location.protocol + "//" + location.host + location.pathname.substr(0, location.pathname.lastIndexOf("/") + 1)
+};
+var article = new Readability(uri, document).parse();
 
-var domContent = document.getElementsByTagName("body")[0].outerHTML;
-var words = domContent.replace(/([ .,;]+)/g,'$1§sep§').split('§sep§');
+//var article = document.getElementsByTagName("body")[0].innerHTML;
+var words = article.content.replace(/([ .,;]+)/g,'$1§sep§').split('§sep§');
 var count = words.length;
-console.log(count);
+// alert(count);
 
 chrome.extension.sendRequest(count);
