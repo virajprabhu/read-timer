4ea5e464c68f3083203af94ef435e1b40ee63e3e
Parameswaran <hsemarap@gmail.com>
2015-10-08 17:40:07 +0530

    time left

--- popup.js
+++ popup.js
@@ -1,10 +1,11 @@
 chrome.extension.onRequest.addListener(function(request) {
-	if(request.type=='count'){
+	if(request.wordLength!=undefined){
 		var wordLength = request.wordLength
 		var averageReadSpeedWPM = 200;
 		var readTime = wordLength / averageReadSpeedWPM;
 		document.getElementById('status').innerHTML= Math.ceil(readTime) + " min read. ";
-	}else if(request.type=='scroll'){
+	}
+	if(request.remainingTime!=undefined){
 			document.getElementById('remaining').innerHTML=request.remainingTime + " left";
 	}
 });
@@ -12,8 +13,7 @@ window.onload = function() {
 	chrome.windows.getCurrent(function (currentWindow) {
 		chrome.tabs.query({ active: true, windowId: currentWindow.id}, function(activeTabs){
 			chrome.tabs.executeScript(null, { file: "readability/Readability.js" }, function() {
-				chrome.tabs.executeScript(
-					activeTabs[0].id, {file: 'sendArticleLength.js', allFrames: true});
+				chrome.tabs.executeScript(activeTabs[0].id, {file: 'sendArticleLength.js', allFrames: true});
 			});
 		});
 	});
--- sendArticleLength.js
+++ sendArticleLength.js
@@ -1,5 +1,5 @@
-
 var location = document.location;
+
 var uri = {
 	spec: location.href,
 	host: location.host,
@@ -11,21 +11,22 @@ var article = new Readability(uri, document).parse();
 
 var words = article.content.replace(/([ .,;]+)/g,'$1§sep§').split('§sep§');
 var count = words.length;
+// var count = 600;
 // alert(count);
 
-
-chrome.extension.sendRequest({type:"count",wordLength:count});
-
-showTimeLeft()
+// chrome.extension.sendRequest({type:"count",wordLength:count});
 
 function showTimeLeft(){
 	var scrollPos = window.pageYOffset | document.body.scrollTop
 	var body = document.body, html = document.documentElement;
 	var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
-	// console.log("Remaining Time: " + Math.ceil((1-scrollPos/height)*count/200 ))
-	chrome.extension.sendRequest({type:"scroll",remainingTime:Math.ceil((1-scrollPos/height)*count/200)});
+	console.log("Remaining Time: " + Math.ceil((1-scrollPos/height)*count/200 ))
+
+	chrome.extension.sendRequest({remainingTime:Math.ceil((1-scrollPos/height)*count/200), wordLength:count});
 }
 
 window.onscroll = function(){
 	showTimeLeft()	
 };
+showTimeLeft()
+
