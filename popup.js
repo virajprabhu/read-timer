38755ef0049afc318898cf5e2efde8fb53f70ea0
Viraj Prabhu <prabhuviraj@gmail.com>
2015-10-08 14:32:00 +0530

    Integrated Readability. @bug: Page reloads

--- popup.js
+++ popup.js
@@ -1,12 +1,7 @@
 chrome.extension.onRequest.addListener(function(wordLength) {
 	var averageReadSpeedWPM = 200;
-<<<<<<< HEAD
 	var readTime = Math.ceil(wordLength/averageReadSpeedWPM);
 	document.getElementById('status').innerHTML = readTime + " minute read.";
-=======
-	var readTime = wordLength / averageReadSpeedWPM;
-	document.getElementById('status').innerHTML='Read Time: ' + Math.ceil(readTime) + " minutes";
->>>>>>> 0db128d9e29709dd0809b1e12da88160af6671aa
 });
 window.onload = function() {
 	chrome.windows.getCurrent(function (currentWindow) {
--- sendArticleLength.js
+++ sendArticleLength.js
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 var location = document.location;
 var uri = {
 	spec: location.href,
@@ -10,12 +9,7 @@ var uri = {
 };
 var article = new Readability(uri, document).parse();
 
-//var article = document.getElementsByTagName("body")[0].innerHTML;
 var words = article.content.replace(/([ .,;]+)/g,'$1§sep§').split('§sep§');
-=======
-var domContent = document.getElementsByTagName("body")[0].outerHTML;
-var words = domContent.replace(/([ .,;]+)/g,'$1§sep§').split('§sep§');
->>>>>>> 0db128d9e29709dd0809b1e12da88160af6671aa
 var count = words.length;
 // alert(count);
 
