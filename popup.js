2c0efd6285cf5f437b81d1679268752c2e455f04
Parameswaran <hsemarap@gmail.com>
2015-10-13 19:14:59 +0530

    bug: doesnt work on scroll

--- popup.js
+++ popup.js
@@ -1,20 +1,33 @@
+var count = -1;
+function totalTime(wc){
+	// alert("Total time "+count)
+	var count = wc;
+	var wordLength = wc;	
+	var averageReadSpeedWPM = 200;
+	var readTime = wordLength / averageReadSpeedWPM;
+	document.getElementById('status').innerHTML= Math.ceil(readTime) + " min read. ";	
+	// setTimeout(function(){
+	// },1000);
+}
 chrome.extension.onRequest.addListener(function(request) {
-	if(request.wordLength!=undefined){
-		var wordLength = request.wordLength
-		var averageReadSpeedWPM = 200;
-		var readTime = wordLength / averageReadSpeedWPM;
-		document.getElementById('status').innerHTML= Math.ceil(readTime) + " min read. ";
-	}
 	if(request.remainingTime!=undefined){
-			document.getElementById('remaining').innerHTML=request.remainingTime + " left";
+		document.getElementById('remaining').innerHTML=request.remainingTime + " left";
 	}
 });
 window.onload = function() {
-	chrome.windows.getCurrent(function (currentWindow) {
-		chrome.tabs.query({ active: true, windowId: currentWindow.id}, function(activeTabs){
-			chrome.tabs.executeScript(null, { file: "readability/Readability.js" }, function() {
-				chrome.tabs.executeScript(activeTabs[0].id, {file: 'sendArticleLength.js', allFrames: true});
+	// chrome.windows.getCurrent(function (currentWindow) {
+		chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs){
+			chrome.tabs.executeScript(activeTabs[0].id, { file: "readability/Readability.js" }, function() {
+				chrome.tabs.executeScript(activeTabs[0].id, {file: 'sendArticleLength.js', allFrames: true}, function(result) {
+					count = result;
+					totalTime(count);					
+					// chrome.windows.getCurrent(function (currentWindow1) {
+						// chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs1){			
+							chrome.tabs.executeScript({file: 'test.js', allFrames: true});		
+						// });
+					// });
+				});
 			});
 		});
-	});
+	// });
 }
--- sendArticleLength.js
+++ sendArticleLength.js
@@ -1,32 +1,33 @@
 var location = document.location;
 
 var uri = {
-	spec: location.href,
-	host: location.host,
-	prePath: location.protocol + "//" + location.host,
-	scheme: location.protocol.substr(0, location.protocol.indexOf(":")),
-	pathBase: location.protocol + "//" + location.host + location.pathname.substr(0, location.pathname.lastIndexOf("/") + 1)
+	spec: document.location.href,
+	host: document.location.host,
+	prePath: document.location.protocol + "//" + document.location.host,
+	scheme: document.location.protocol.substr(0, document.location.protocol.indexOf(":")),
+	pathBase: document.location.protocol + "//" + document.location.host + document.location.pathname.substr(0, document.location.pathname.lastIndexOf("/") + 1)
 };
-var article = new Readability(uri, document).parse();
+// var body = document.getElementsByTagName("body")[0].innerHTML;
+// console.log(body);
+/*function clone(obj) {
+    if (null == obj || "object" != typeof obj) return obj;
+    var copy = obj.constructor();
+    for (var attr in obj) {
+        if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+    }
+    return copy;
+}
 
+var dupDoc = clone(document);*/
+var article = new Readability(uri, document).parse();
 var words = article.content.replace(/([ .,;]+)/g,'$1§sep§').split('§sep§');
 var count = words.length;
+// alert('count' + count);
+count
+
+// document.getElementsByTagName("body")[0].innerHTML = body; // to rectify the wierd bug caused by Readability where the page elements get removed.
+
 // var count = 600;
 // alert(count);
 
 // chrome.extension.sendRequest({type:"count",wordLength:count});
-
-function showTimeLeft(){
-	var scrollPos = window.pageYOffset | document.body.scrollTop
-	var body = document.body, html = document.documentElement;
-	var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
-	console.log("Remaining Time: " + Math.ceil((1-scrollPos/height)*count/200 ))
-
-	chrome.extension.sendRequest({remainingTime:Math.ceil((1-scrollPos/height)*count/200), wordLength:count});
-}
-
-window.onscroll = function(){
-	showTimeLeft()	
-};
-showTimeLeft()
-
