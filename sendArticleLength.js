e4bc93454cae987b644c57b36471694bbcf374b1
Parameswaran <hsemarap@gmail.com>
2015-10-14 15:38:58 +0530

    Total time

--- sendArticleLength.js
+++ sendArticleLength.js
@@ -7,27 +7,8 @@ var uri = {
 	scheme: document.location.protocol.substr(0, document.location.protocol.indexOf(":")),
 	pathBase: document.location.protocol + "//" + document.location.host + document.location.pathname.substr(0, document.location.pathname.lastIndexOf("/") + 1)
 };
-// var body = document.getElementsByTagName("body")[0].innerHTML;
-// console.log(body);
-/*function clone(obj) {
-    if (null == obj || "object" != typeof obj) return obj;
-    var copy = obj.constructor();
-    for (var attr in obj) {
-        if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
-    }
-    return copy;
-}
 
-var dupDoc = clone(document);*/
 var article = new Readability(uri, document).parse();
 var words = article.content.replace(/([ .,;]+)/g,'$1§sep§').split('§sep§');
 var count = words.length;
-// alert('count' + count);
 count
-
-// document.getElementsByTagName("body")[0].innerHTML = body; // to rectify the wierd bug caused by Readability where the page elements get removed.
-
-// var count = 600;
-// alert(count);
-
-// chrome.extension.sendRequest({type:"count",wordLength:count});
--- test.js
+++ test.js
@@ -1,21 +1,13 @@
-
- // alert("Send article length "+count)
-// document.getElementsByTagName("body")[0].innerHTML = "SDFSDF";
 function showTimeLeft(){
 	var scrollPos = window.pageYOffset | document.body.scrollTop
 	var body = document.body, html = document.documentElement;
 	var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
 	console.log("Remaining Time: " + Math.ceil((1-scrollPos/height)*count/200 ))
-    // alert("Remaining Time: " + Math.ceil((1-scrollPos/height)*count/200 ));
 	chrome.extension.sendRequest({remainingTime:Math.ceil((1-scrollPos/height)*count/200)});
 }
-// var location = document.location;
-
 window.onscroll = function(){
 	showTimeLeft()	
 };
-
-
 showTimeLeft()
 
 
